8d35fffe3674f8b56355f767922415d60c366bda
TheGazdaman <michal.gazdik@livesport.eu>
2021-03-20 18:33:24 +0100

    control input value

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,28 +4,24 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-    const [error, setError] = React.useState(null);
-    const usernameInputRef = React.useRef();
+    const [username, setUsername] = React.useState('');
 
     function handleSubmit(event) {
         event.preventDefault();
-        const value = usernameInputRef.current.value;
-        onSubmitUsername(value);
+        onSubmitUsername(username);
     }
 
     function handleChange(event) {
         const {value} = event.target;
-        const isLowerCase = value === value.toLowerCase();
-        setError(isLowerCase ? null : "username must be lowercase")
+        setUsername(value.toLowerCase());
     }
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input ref={usernameInputRef} id="usernameInput" type="text" onChange={handleChange} />
+        <input id="usernameInput" type="text" onChange={handleChange} value={username} />
       </div>
-        <div style={{color: 'red'}}>{error}</div>
-      <button disabled={Boolean(error)} type="submit">Submit</button>
+      <button type="submit">Submit</button>
     </form>
   )
 }
